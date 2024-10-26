ed0184351fe63a369098b5e52c92743c40246c25
Rodin Sh <65271515+Rodin-Sh@users.noreply.github.com>
2024-10-26 01:48:47 -0700

    New chatbot format using streamlit
    Taught me how to make chatbots! Context is important.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,35 @@
 import streamlit as st
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-client = OpenAI()
+load_dotenv()
+API_KEY = os.getenv("OPENAI_API_KEY")
 
-st.title("Climate Chatbot")
-user_message = st.text_input("Message", "")
+with st.sidebar:
+    openai_api_key = API_KEY
+    # openai_api_key = st.text_input("OpenAI API Key", type="password")
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are an expert in climate science. You also have knowledge of climate fiction novels, like Ministry of the Future of the Fifth Season."},
-        {
-            "role": "user",
-            "content": f"{user_message}"
-        }
-    ]
-)
+client = OpenAI(api_key=openai_api_key)
 
-if user_message:
-    st.write(completion.choices[0].message.content)
+st.title("🌎 Seneca")
+st.caption("The Minister of the Present")
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I assist you today?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please provide OpenAI API Key")
+        st.stop()
+    
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-4o-mini", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
